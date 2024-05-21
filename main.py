1e269406ed3f9cc32c50db9563959e6aadafacdf
jenhil0809 <jenhil0809@highgateschool.org.uk>
2024-05-21 10:15:06 +0100

    Created properties and added choice for number of guesses and language

--- main.py
+++ main.py
@@ -4,53 +4,61 @@ from colorama import Fore
 
 
 class Wordle:
-    def __init__(self, length: int = 5):
+    def __init__(self, length: int = 5, guess_num: int = 6, lang="en"):
         self.length = length
-        self.common_words = [word for word in top_n_list('en', 10000)
+        self.lang = lang
+        self.common_words = [word for word in top_n_list(self.lang, 10000)
                              if len(word) == self.length and "'" not in word]
-        self.secret_word = ""
+        self._secret_word = ""
         self.set_secret_word()
-        self.guessed_word = ""
-        self.game = Game(self)
+        self._guessed_word = " "*self.length
+        self.game = Game(self, guess_num)
 
     def set_secret_word(self):
-        self.secret_word = choice(self.common_words)
+        self._secret_word = choice(self.common_words)
+
+    def get_secret_word(self):
+        return self._secret_word
 
     def set_guessed_word(self, word):
-        self.guessed_word = word
+        self._guessed_word = word
 
+    @property
     def matches(self):
         lst = []
         for i in range(self.length):
-            if self.guessed_word[i] == self.secret_word[i]:
-                lst.append((self.guessed_word[i], 2))
-            elif self.guessed_word[i] in self.secret_word:
-                lst.append((self.guessed_word[i], 1))
+            if self._guessed_word[i] == self._secret_word[i]:
+                lst.append((self._guessed_word[i], 2))
+            elif self._guessed_word[i] in self._secret_word:
+                lst.append((self._guessed_word[i], 1))
             else:
-                lst.append((self.guessed_word[i], 0))
-        return self.guessed_word == self.secret_word, lst
+                lst.append((self._guessed_word[i], 0))
+        return lst
+
+    @property
+    def finished(self):
+        return self._guessed_word == self._secret_word
 
 
 class Game:
-    def __init__(self, master, guess_num: int = 6):
+    def __init__(self, master: Wordle, guess_num: int = 6):
         self.master = master
         self.guess_num = guess_num
         self.guesses = []
         self.interface = Interface()
-        self.play_game()
 
     def play_game(self):
-        self.get_guess()
-        while len(self.guesses) < self.guess_num and not self.master.matches()[0]:
-            self.get_guess()
-        print(f"Answer={self.master.secret_word}\n\n")
-        self.reset_game()
-
-    def get_guess(self):
-        guess = self.interface.get_guess(self.master.length, self.guesses)
+        guess = self.interface.get_guess(self.master.length, self.guesses, self.master.lang)
         self.guesses.append(guess.upper())
         self.master.set_guessed_word(guess)
-        self.interface.print_guess(guess, self.master.matches()[1])
+        self.interface.print_guess(guess, self.master.matches)
+        while len(self.guesses) < self.guess_num and not self.master.finished:
+            guess = self.interface.get_guess(self.master.length, self.guesses, self.master.lang)
+            self.guesses.append(guess.upper())
+            self.master.set_guessed_word(guess)
+            self.interface.print_guess(guess, self.master.matches)
+        print(f"Answer={self.master.get_secret_word()}\n\n")
+        self.reset_game()
 
     def reset_game(self):
         self.guesses = []
@@ -73,13 +81,15 @@ class Interface:
                 print(f"{Fore.GREEN}{guess[i].upper()}{Fore.WHITE}", end="")
         print("\n")
 
-    def get_guess(self, length, guesses):
+    @staticmethod
+    def get_guess(length, guesses, lang):
         guess = ""
-        while (len(guess) != length or zipf_frequency(guess, "en") == 0.0 or
+        while (len(guess) != length or zipf_frequency(guess, lang) == 0.0 or
                guess.upper() in guesses or not guess.isalpha()):
             guess = input("Guess: ")
         return guess
 
 
 if __name__ == "__main__":
-    game = (Wordle(5))
+    game = Wordle(7, 6, "en")
+    game.game.play_game()
